fd879b0fb62f2f4caed667385ec685d148cd063f
Uwe Jugel <uwe.jugel@googlemail.com>
2011-08-17 10:50:02 +0200

    adding self test to build.js

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
-hcbrew = require("./.hotcoffee/tools/hotcoffee.js")
+hcbrew = require("./.hotcoffee/tools/hotcoffee.js") // load hcb
+hcbrew.buildAll()                                   // build hcb and project
+// hcbrew.test("./src/test.co")                       // run project tests
 
-hcbrew.buildAll()
-hcbrew.test("./src/test.co")
-
+hctest = require("./.hotcoffee/tmp_tastecoffee.js") // hcb selftest - You may remove this
